42f552db28721bf92876e3ec449f303e84e7653e
Adam Miller <admiller@redhat.com>
2017-12-21 16:42:17 -0600

    first run at a reproducer for the openshift-ops memory leak issue in 2.4.1
    Signed-off-by: Adam Miller <admiller@redhat.com>

---
+++ openshift-ops-2.4-memleak/tools_roles/lib_another_role/library/somelibrarymod.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+'''
+pointless module to test with
+
+'''
+from ansible.module_utils.basic import AnsibleModule
+
+
+def main():
+    '''
+    ansible another_rolemod module
+    '''
+    module = AnsibleModule(
+        argument_spec=dict(
+            something_to_say=dict(default=None, required=True, type='str'),
+        ),
+    )
+
+    module.exit_json(msg=module.params["something_to_say"])
+
+
+if __name__ == "__main__":
+    main()
